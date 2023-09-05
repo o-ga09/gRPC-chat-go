393d46cab23f71a1c2288837567865a85ef36a28
taiti09 <taitiabe1997@gmail.com>
2023-09-05 23:53:09 +0900

    redis pub/sub

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "gRPC-chat/server"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/go-redis/redis"
+)
 
 func main() {
-	server.Run()
+	// server.Run()
+	redisdb := redis.NewClient(&redis.Options{
+		Addr:         ":6379",
+		DialTimeout:  10 * time.Second,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		PoolSize:     10,
+		PoolTimeout:  30 * time.Second,
+	})
+
+	pubsub := redisdb.Subscribe("testchannel")
+
+	// Wait for confirmation that subscription is created before publishing anything.
+	_, err := pubsub.Receive()
+	if err != nil {
+		panic(err)
+	}
+
+	// Go channel which receives messages.
+	go func(){
+		for {
+			ch := pubsub.Channel()
+			// Consume messages.
+			for msg := range ch {
+				fmt.Println(msg.Channel, msg.Payload)
+			}
+		}
+	}()
+
+	quit := make(chan os.Signal,1)
+	signal.Notify(quit,os.Interrupt)
+	<- quit
+	log.Printf("chat stopping ...")
+
+	time.AfterFunc(time.Second, func() {
+		// When pubsub is closed channel is closed too.
+		_ = pubsub.Close()
+	})
 }
